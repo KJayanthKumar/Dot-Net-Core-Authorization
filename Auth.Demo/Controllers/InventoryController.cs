3f3ab0e25febe88530b45074c481771b095e7c41
K <JK063102@cerner.net>
2021-02-17 23:01:12 -0600

    Authorization

---
+++ Auth.Demo/Controllers/InventoryController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Auth.Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        // GET: api/<InventoryController>
+        [Authorize(Roles = "Administrator, User")]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [Authorize(Roles = "Administrator")]
+        // POST api/<InventoryController>
+        [HttpPost]
+        public void Post([FromBody] Inventory value)
+        {
+        }
+
+    }
+}
---
+++ Auth.Demo/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Auth.Demo
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
+    }
+}
